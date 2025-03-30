0921745bf6a9fed3fc5d7b9c42d3f2e481f43997
agustinn-dev <agustinpenelas@gmail.com>
2025-03-29 22:30:01 -0300

    Remove banner debug in application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:converter_money/home.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -9,11 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
